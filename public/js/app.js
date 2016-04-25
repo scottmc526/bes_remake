e97dbfdd03aee96542058a2e843970cf4f8f74be
Scott Christensen <scottmc526@gmail.com>
2016-04-25 16:53:29 -0600

    firebase and angular set up

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+var app = angular.module('bes', ['ngRoute'])
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/home', {
+      templateUrl: 'partials/home.html',
+      controller: 'mainController'
+    })
+})
